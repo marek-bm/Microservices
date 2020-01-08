1d0ff005ac981fc62c48405f02636a20277b17f4
Marek Jastrzębski <marek.jastrzebski@outlook.com>
2020-01-08 16:51:34 +0100

    commons

---
+++ 01-commons/src/main/java/model/Customer.java
@@ -0,0 +1,48 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
+    String firstName;
+    String lastName;
+    String driveLicenseNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDriveLicenseNumber() {
+        return driveLicenseNumber;
+    }
+
+    public void setDriveLicenseNumber(String driveLicenseNumber) {
+        this.driveLicenseNumber = driveLicenseNumber;
+    }
+}
---
+++ 01-commons/src/main/java/model/Vehicle.java
@@ -0,0 +1,48 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
+    String model;
+    String make;
+    int year;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
