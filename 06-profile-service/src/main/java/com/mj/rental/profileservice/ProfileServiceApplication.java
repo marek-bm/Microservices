e5b0978d2e8f5cabb80c907fddbfd08ee662b4c6
marek <marek.jastrzebski@outlook.com>
2020-02-27 21:37:02 +0100

    Resource server enabled

--- 06-profile-service/src/main/java/com/mj/rental/profileservice/ProfileServiceApplication.java
+++ 06-profile-service/src/main/java/com/mj/rental/profileservice/ProfileServiceApplication.java
@@ -3,9 +3,11 @@ package com.mj.rental.profileservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EntityScan(basePackages = "model")
+@EnableResourceServer
 public class ProfileServiceApplication {
 
     public static void main(String[] args) {
