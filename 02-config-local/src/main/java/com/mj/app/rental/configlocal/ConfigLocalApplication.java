5726eb766b0919fd2af420867025377493ba4e1c
marek <marek.jastrzebski@outlook.com>
2020-02-28 21:58:18 +0100

    Changes in config files

--- 02-config-local/src/main/java/com/mj/app/rental/configlocal/ConfigLocalApplication.java
+++ 02-config-local/src/main/java/com/mj/app/rental/configlocal/ConfigLocalApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigLocalApplication {
+public class    ConfigLocalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigLocalApplication.class, args);
